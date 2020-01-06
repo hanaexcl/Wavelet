f563359e09766a7dc6e459c7729d314503e2d76e
Kusyun <kusyunexcl@gmail.com>
2020-01-07 07:12:42 +0800

    加入專案檔案。

---
+++ Wavelet/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Wavelet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 530);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(530, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 74);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Haar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(676, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1206, 1056);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Wavelet";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Wavelet/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Wavelet
+{
+    struct RGB
+    {
+        public int R;
+        public int G;
+        public int B;
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int _haarTimes = 4;
+
+            Bitmap tmpBitmap = new Bitmap("C:\\Users\\Kusyun_home2\\Desktop\\lena_gray.bmp");
+            Bitmap ntmpBitmap = new Bitmap(tmpBitmap.Width, tmpBitmap.Height);
+            Bitmap ttmpBitmap = new Bitmap(tmpBitmap.Width, tmpBitmap.Height);
+
+            pictureBox1.Image = tmpBitmap;
+
+            int tmpSize_W = (int)Math.Pow(2, Math.Ceiling(Math.Log(tmpBitmap.Width, 2)));
+            int tmpSize_H = (int)Math.Pow(2, Math.Ceiling(Math.Log(tmpBitmap.Height, 2)));
+            RGB[,] img = new RGB[tmpSize_W, tmpSize_H];
+
+
+            int r, g, b;
+            for (int h = 0; h < tmpSize_H; h++)
+            {
+                for (int w = 0; w < tmpSize_W; w++)
+                {
+                    r = 0; g = 0; b = 0;
+                    if (w < tmpBitmap.Width && h < tmpBitmap.Height)
+                    {
+                        r = tmpBitmap.GetPixel(w, h).R;
+                        g = tmpBitmap.GetPixel(w, h).G;
+                        b = tmpBitmap.GetPixel(w, h).B;
+                    }
+
+                    RGB tmpRGB = new RGB();
+                    tmpRGB.R = r;
+                    tmpRGB.G = g;
+                    tmpRGB.B = b;
+
+                    img[w, h] = tmpRGB;
+                }
+            }
+
+            //計算
+            int harrtmpSize_H = tmpSize_H;
+            int harrtmpSize_W = tmpSize_W;
+            for (int harrTimes = 1; harrTimes <= _haarTimes; harrTimes ++)
+            {
+                //橫
+                for (int h = 0; h < harrtmpSize_H; h++)
+                {
+                    int runTimes = 0;
+                    for (int t = tmpSize_W; t != 1; t = t / 2)
+                    {
+                        runTimes++;
+                        if (runTimes < harrTimes) continue;
+
+                        List<RGB> tmp = new List<RGB>();
+                        for (int i = 0; i < t; i = i + 2)
+                        {
+                            img[i / 2, h].R = (img[i, h].R + img[i + 1, h].R) / 2;
+                            img[i / 2, h].G = (img[i, h].G + img[i + 1, h].G) / 2;
+                            img[i / 2, h].B = (img[i, h].B + img[i + 1, h].B) / 2;
+
+                            RGB tmpRGB = new RGB();
+                            tmpRGB.R = img[i / 2, h].R - img[i + 1, h].R;
+                            tmpRGB.G = img[i / 2, h].G - img[i + 1, h].G;
+                            tmpRGB.B = img[i / 2, h].B - img[i + 1, h].B;
+                            tmp.Add(tmpRGB);
+                        }
+                        tmp.Reverse();
+
+                        for (int i = 0; i < tmp.Count; i++)
+                        {
+                            img[t - i - 1, h] = tmp[i];
+                            //img[t - i - 1, h].R = ((img[t - i - 1, h].R * 8) + 128);
+                            //img[t - i - 1, h].G = ((img[t - i - 1, h].G * 8) + 128);
+                            //img[t - i - 1, h].B = ((img[t - i - 1, h].B * 8) + 128);
+                        }
+
+                        break;
+                    }
+                }
+
+                //列
+                for (int w = 0; w < harrtmpSize_W; w++)
+                {
+                    int runTimes = 0;
+                    for (int t = tmpSize_H; t != 1; t = t / 2)
+                    {
+                        runTimes++;
+                        if (runTimes < harrTimes) continue;
+
+                        List<RGB> tmp = new List<RGB>();
+                        for (int i = 0; i < t; i = i + 2)
+                        {
+                            img[w, i / 2].R = (img[w, i].R + img[w, i + 1].R) / 2;
+                            img[w, i / 2].G = (img[w, i].G + img[w, i + 1].G) / 2;
+                            img[w, i / 2].B = (img[w, i].B + img[w, i + 1].B) / 2;
+
+                            RGB tmpRGB = new RGB();
+                            tmpRGB.R = img[w, i / 2].R - img[w, i + 1].R;
+                            tmpRGB.G = img[w, i / 2].G - img[w, i + 1].G;
+                            tmpRGB.B = img[w, i / 2].B - img[w, i + 1].B;
+
+                            tmp.Add(tmpRGB);
+                        }
+                        tmp.Reverse();
+
+                        for (int i = 0; i < tmp.Count; i++)
+                        {
+                            img[w, t - i - 1] = tmp[i];
+                            //img[w, t - i - 1].R = ((img[w, t - i - 1].R * 8) + 128);
+                            //img[w, t - i - 1].G = ((img[w, t - i - 1].G * 8) + 128);
+                            //img[w, t - i - 1].B = ((img[w, t - i - 1].B * 8) + 128);
+                        }
+
+                        break;
+                    }
+                }
+
+                harrtmpSize_H = harrtmpSize_H / 2;
+                harrtmpSize_W = harrtmpSize_W / 2;
+            }
+
+            //去除最小值
+            for (int i = 0; i < 10; i++)
+            {
+                int minR, minG, minB;
+                minR = img[0, 0].R;
+                minG = img[0, 0].G;
+                minB = img[0, 0].B;
+
+                for (int h = 0; h < ntmpBitmap.Height; h++)
+                {
+                    for (int w = 0; w < ntmpBitmap.Width; w++)
+                    {
+                        if (Math.Abs(minR) > Math.Abs(img[w, h].R) && img[w, h].R > 0) minR = img[w, h].R;
+                        if (Math.Abs(minG) > Math.Abs(img[w, h].G) && img[w, h].G > 0) minG = img[w, h].G;
+                        if (Math.Abs(minB) > Math.Abs(img[w, h].B) && img[w, h].B > 0) minB = img[w, h].B;
+                    }
+                }
+
+                for (int h = 0; h < ntmpBitmap.Height; h++)
+                {
+                    for (int w = 0; w < ntmpBitmap.Width; w++)
+                    {
+                        if (img[w, h].R == minR) img[w, h].R = 0;
+                        if (img[w, h].G == minG) img[w, h].G = 0;
+                        if (img[w, h].B == minB) img[w, h].B = 0;
+                    }
+                }
+            }
+
+            //顯示
+            for (int h = 0; h < ntmpBitmap.Height; h++)
+            {
+                for (int w = 0; w < ntmpBitmap.Width; w++)
+                {
+                    r = img[w, h].R; g = img[w, h].G; b = img[w, h].B;
+
+                    //if (r < 0) r = r + 256;
+                    //if (g < 0) g = g + 256;
+                    //if (b < 0) b = b + 256;
+
+                    //if (r < 0) r = Math.Abs(r);
+                    //if (g < 0) g = Math.Abs(g);
+                    //if (b < 0) b = Math.Abs(b);
+                    //int avg = (r + g + b) / 3;
+
+                    //r = r * 8 + 128;
+                    //g = g * 8 + 128;
+                    //b = b * 8 + 128;
+
+                    if (h >= harrtmpSize_H || w >= harrtmpSize_W)
+                    {
+                        r = ((r * 8) + 128);
+                        g = ((g * 8) + 128);
+                        b = ((b * 8) + 128);
+                    }
+
+                    if (r < 0) r = 0;
+                    if (r > 255) r = 255;
+                    if (g < 0) g = 0;
+                    if (g > 255) g = 255;
+                    if (b < 0) b = 0;
+                    if (b > 255) b = 255;
+                    //ntmpBitmap.SetPixel(w, h, Color.FromArgb(avg, avg, avg));
+
+                    ntmpBitmap.SetPixel(w, h, Color.FromArgb(r, g, b));
+                }
+            }
+
+            pictureBox2.Image = ntmpBitmap;
+
+
+            //回推
+            for (int harrTimes = _haarTimes; harrTimes >= 1; harrTimes--)
+            {
+                //列
+                for (int w = 0; w < tmpSize_W; w++)
+                {
+                    int middle = (int)tmpSize_H / (int)Math.Pow(2, harrTimes);
+
+                    RGB[] tmpList = new RGB[tmpSize_H];
+
+                    for (int h = 0; h < middle; h++)
+                    {
+                        tmpList[h * 2].R = img[w, h].R + img[w, middle + h].R;
+                        tmpList[h * 2].G = img[w, h].G + img[w, middle + h].G;
+                        tmpList[h * 2].B = img[w, h].B + img[w, middle + h].B;
+
+                        tmpList[h * 2 + 1].R = img[w, h].R - img[w, middle + h].R;
+                        tmpList[h * 2 + 1].G = img[w, h].G - img[w, middle + h].G;
+                        tmpList[h * 2 + 1].B = img[w, h].B - img[w, middle + h].B;
+                    }
+
+                    for (int h = 0; h < tmpList.Length; h++)
+                    {
+                        img[w, h] = tmpList[h];
+                    }
+                }
+
+                //橫
+                for (int h = 0; h < tmpSize_H; h++)
+                {
+                    int middle = (int)tmpSize_W / (int)Math.Pow(2, harrTimes);
+
+                    RGB[] tmpList = new RGB[tmpSize_W];
+
+                    for (int w = 0; w < middle; w++)
+                    {
+                        tmpList[w * 2].R = img[w, h].R + img[middle + w, h].R;
+                        tmpList[w * 2].G = img[w, h].G + img[middle + w, h].G;
+                        tmpList[w * 2].B = img[w, h].B + img[middle + w, h].B;
+
+                        tmpList[w * 2 + 1].R = img[w, h].R - img[middle + w, h].R;
+                        tmpList[w * 2 + 1].G = img[w, h].G - img[middle + w, h].G;
+                        tmpList[w * 2 + 1].B = img[w, h].B - img[middle + w, h].B;
+                    }
+
+                    for (int w = 0; w < tmpList.Length; w++)
+                    {
+                        img[w, h] = tmpList[w];
+                    }
+                }
+
+
+                //harrtmpSize_H = harrtmpSize_H * 2;
+                //harrtmpSize_W = harrtmpSize_W * 2;
+            }
+
+            //顯示
+            for (int h = 0; h < ttmpBitmap.Height; h++)
+            {
+                for (int w = 0; w < ttmpBitmap.Width; w++)
+                {
+                    r = img[w, h].R; g = img[w, h].G; b = img[w, h].B;
+
+                    if (r < 0) r = 0;
+                    if (r > 255) r = 255;
+                    if (g < 0) g = 0;
+                    if (g > 255) g = 255;
+                    if (b < 0) b = 0;
+                    if (b > 255) b = 255;
+
+                    ttmpBitmap.SetPixel(w, h, Color.FromArgb(r, g, b));
+                }
+            }
+
+            pictureBox3.Image = ttmpBitmap;
+        }
+    }
+}
